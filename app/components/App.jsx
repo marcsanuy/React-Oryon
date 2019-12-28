323a9cdfd869a354d5e7935cd378731558d198ec
marcsanuy <marcsanuy@gmail.com>
2019-12-28 13:12:20 +0100

    React and Flux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
+import connect from '../libs/connect';
 
-export default class App extends React.Component {
+
+class App extends React.Component {
   constructor(props) {
     super(props);
 
@@ -52,6 +54,7 @@ export default class App extends React.Component {
 
     return (
       <div>
+        {this.props.test}
         <button className="add-note" onClick={this.addNote}>+</button>
         <Notes
           notes={notes}
@@ -103,3 +106,7 @@ export default class App extends React.Component {
     });
   }
 }
+
+export default connect(() => ({
+  test: 'testing'
+}))(App)
