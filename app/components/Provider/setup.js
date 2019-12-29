c8b332f48b83438961c56b72df3f335be710ab38
marcsanuy <marcsanuy@gmail.com>
2019-12-29 20:19:07 +0100

    Implementing Persistence in localStorage

--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -1,5 +1,9 @@
 import NoteStore from '../../stores/NoteStore';
+import persist from '../../libs/persist';
+import storage from '../../libs/storage';
 
 export default alt => {
     alt.addStore('NoteStore', NoteStore);
+
+    persist(alt, storage(localStorage), 'app');
 }
